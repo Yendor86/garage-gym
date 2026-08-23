50670eeb29c109006a1e32840e66e45ed837a583
Yendor86 <rodneybaker@outlook.com.au>
2026-08-23 13:49:33 -0700

    Inject play boot splash if markup missing

--- js/write-guard.js
+++ js/write-guard.js
@@ -4,14 +4,27 @@
   window.GG_PRACTICE = false;
   window.GG_PLAY = true;
   window.GG_STORE_HOUSEHOLD = 'GYM-EHDT2C';
-  window.GG_LIVE_PROJECT = 'cksnuiqpobgrdkotnrdm'; // do not write this project
+  window.GG_LIVE_PROJECT = 'cksnuiqpobgrdkotnrdm';
   window.GG_PLAY_PROJECT = 'sqvybphogacjcqesktos';
   window.GG_PRACTICE_KEY = 'garageGymTracker_play_v1';
 })();
 
 (function(){
+  if(!document.querySelector('link[href="css/boot.css"]')){
+    var l=document.createElement('link');
+    l.rel='stylesheet';
+    l.href='css/boot.css';
+    document.head.appendChild(l);
+  }
   var el=document.getElementById('bootSplash');
-  if(!el) return;
+  if(!el){
+    el=document.createElement('div');
+    el.id='bootSplash';
+    el.className='bootsplash';
+    el.setAttribute('aria-hidden','true');
+    el.innerHTML='<img src="icon-1024.png" alt="">';
+    document.body.insertBefore(el, document.body.firstChild);
+  }
   if(sessionStorage.getItem('ggBooted')){ el.classList.add('go'); return; }
   sessionStorage.setItem('ggBooted','1');
   setTimeout(function(){ el.classList.add('go'); }, 900);
